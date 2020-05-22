6dacf849bdb0401a1de8a104062111bff72f027d
Cherukkattil Naseer <naseer@Cherukkattils-MacBook-Pro.local>
2020-05-21 22:35:17 -0400

    First commit

---
+++ src/jokeGenerator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Axios from 'axios';
+
+export default class JokeGenerator extends React.Component {
+    state = {
+        joke: null,
+        loading: false
+    };
+
+    loadJoke = async () => {
+        this.setState({ loading:true });
+
+        const { data: { value: { joke }}} = await Axios.get("http://api.icndb.com/jokes/random");
+
+        this.setState({ loading: false, joke });
+    };
+
+    render() {
+        const { joke, loading } = this.state;
+
+        return ( <React.Fragment>
+            { !joke && <div>You haven't loaded any joke yet!</div>}
+            {loading && <div>Loading...</div>}
+            {joke && !loading && <Joke text = {joke} />}
+
+            <button onClick={this.loadJoke} type="submit">
+                Load a random joke
+            </button>
+        </React.Fragment>
+        );
+    }
+ }
+
